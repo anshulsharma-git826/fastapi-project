333c84be308e7785f5f9e2342963cb47614dc485
anshulsharma-git826 <anshul3579sharma@gmail.com>
2026-03-29 12:14:37 +0530

    fix all database import typeos

--- auth.py
+++ auth.py
@@ -48,7 +48,7 @@ def get_current_user(
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: str = payload.get("sub")
+        user_id: str = payload.get("user_id")
 
         if user_id is None:
             raise credentials_exception
